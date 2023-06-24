1ba99f9e3eea50e2f3013848ff7c86005b55149c
TarunVanshkar <tarunben33@gmail.com>
2023-06-24 21:32:22 +0530

    submitting code-Sat Jun 24 2023 21:32:16 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,46 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from 'react'
+import Button from './Button';
 
+const App = () => {
+
+    const buttons = ["C", "/", "*", "-", "7", "8", "9", "+", "4", "5", "6", "1", "2", "3", "=", "0", ".", ""]
+    let [solution, setSolution] = useState("");
+
+    function handleClick(id) {
+        if (id === "=") {
+            try {
+                console.log(eval(solution));
+                if (eval(solution) != "undefined") {
+                    setSolution(eval(solution))
+                }
+            } catch (error) {
+                setSolution("Error");
+            }
+
+
+        } else if (id == "C") {
+            setSolution("");
+        } else {
+            setSolution(() => solution + id);
+        }
+    }
+
+    console.log(solution);
+    return (
+        <div className='container Calculator'>
+            <div className='expression' id="calci">{solution}</div>
+            <div className="button-container">
+                {
+                    buttons.map((button) => (
+                        <Button id={button} handleClick={handleClick}></Button>
+                    ))
+                }
+            </div>
+        </div>
+
+
+    )
+}
+
+
+export default App;
